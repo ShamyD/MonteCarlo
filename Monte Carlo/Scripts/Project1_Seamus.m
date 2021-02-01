30e4769cda3fc7771c3667230f93bc1e1338d9f0
ShamyD <Seamus.D96@gmail.com>
2021-02-01 17:47:41 +0100

    asdsad

--- Monte Carlo/Scripts/Project1_Seamus.m
+++ Monte Carlo/Scripts/Project1_Seamus.m
@@ -35,12 +35,30 @@ for i=1:N
     V = wblinv(prob, lambda, k);
     power_t(i, :) = P(V);
 end
-M = N./(Fb-Fa) %Expected number of draws with 0 included
+M = N./(Fb-Fa); %Expected number of draws with 0 included
 tau_nt = sum(power_t)./M;
 tau_nt
 
 sigma_t = std(power_t);
-I_t = 2*(1.96./sqrt(M)).*sigma_t;
+I_t = 2*(1.96./sqrt(M)).*sigma_t
+
+%% Monte Carlo on truncated distribution (IS framework)
+N = 10^4;
+Fb = wblcdf(25, lambda, k);
+Fa = wblcdf(3.5, lambda, k);
+
+for i=1:N
+    R = rand(1, 12);
+    prob = R.*(Fb-Fa)+Fa;
+    V = wblinv(prob, lambda, k);
+    power_t(i, :) = P(V)'.*(Fb-Fa);
+end
+
+tau_ntIS = sum(power_t)./N;
+tau_ntIS
+
+sigma_tIS = std(power_t);
+I_tIS = 2*(1.96./sqrt(N)).*sigma_tIS
 
 %% Importance sampling
 N = 10^4;
@@ -76,8 +94,57 @@ my = x(I);
 
 n = @(y) normpdf(y, my, 5);
 % plot(x, M*n(x)/n(my))
-%%
-plot(x, fi(x)'.*f(x)./n(x))
+%% Antithetic sampling
+N = 10^4;
+Fb = wblcdf(25, lambda, k);
+Fa = wblcdf(3.5, lambda, k);
+
+for i=1:N/2
+    U = rand(1, 12);
+    Uhat = 1-U;
+    prob1 = U.*(Fb-Fa)+Fa;
+    prob2 = Uhat.*(Fb-Fa)+Fa;
+    V1 = P(wblinv(prob1, lambda, k))'.*(Fb-Fa);
+    V2 = P(wblinv(prob2, lambda, k))'.*(Fb-Fa);
+    
+    power(i,:) = (V1 + V2)/2;
+end
+
+tau_AS = mean(power);
+I_AS = 2*norminv(0.975)*std(power)/sqrt(N/2);
+
+tau_AS
+I_AS
+
+%% E(Ptot)
+Ptot = @(x) (1/8)*1.225*pi*(164^2)*(x.^3);
+
+N = 10^4;
+
+for i = 1:N
+   pow(i,:) = Ptot(wblrnd(lambda, k));
+end
+
+E = mean(pow); %E is meant to be produced analytically
+
+kvot = tau_nis./E; %IS gave the best result
+I = I_is/E;
+I;
+
+%% Capacity and Availablity
+
+tau = tau_nis;
+MAX_P = 9.5e6;
+capacity_factor = tau/MAX_P; %REALLY GOOD (especially in winter)
+
+Fb = wblcdf(25, lambda, k);
+Fa = wblcdf(3.5, lambda, k);
+
+availability_factor = Fb-Fa %Worse, always lower than 
+
+%% FRÅGOR 
+%Skillnad mellan truncerings-algortimerna? - är de samma sak?
+%Kan vi trunkera på sättet vi gjorde först?
 
 
-
+%IS bättre än AS?
