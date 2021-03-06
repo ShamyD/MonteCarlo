9845daddc1d32e3f9a1cfcc00d975c1f8794c2dd
Elias Olson <elias973@hotmail.com>
2021-03-06 12:44:15 +0100

    Q2 done

--- Monte Carlo/Scripts/Proj3/Q2.m
+++ Monte Carlo/Scripts/Proj3/Q2.m
@@ -12,14 +12,33 @@ wh = zeros(B,n);
 mu_star = zeros(B,1);
 beta_star = zeros(B,1);
 
+F_inv_T_star = zeros(B,1);
+T = 3*14*100;
+F_inv_T = gumb_inv(mu, beta, (1-1/T));
+
+
 for i=1:B
     
-    wh(i,:) = gumb_inv(r(i,:));
+    wh(i,:) = gumb_inv(mu,beta,r(i,:));
+    [beta_star(i), mu_star(i)] = est_gumbel(wh(i,:));
     
-    [mu_star(i), beta_star(i)] = est_gumbel(wh(i,:));
+    % Calc 100-year return value with the estimated parameters.
+    F_inv_T_star(i) = gumb_inv(mu_star(i), beta_star(i), (1-1/T));
     
 end
 
-% Skapa delta_mu och delta_beta.
-% Skapa left bound and right bound for both.
+mu_diff = sort(mu_star - mu);
+beta_diff = sort(beta_star - beta);
+
+F_inv_T_diff = sort(F_inv_T_star - F_inv_T);
+
+alpha = 0.05;
+
+mu_conf = [mu - mu_diff(floor((1-alpha/2)*B)), mu - mu_diff(ceil(alpha/2*B))]
+beta_conf = [beta - beta_diff(floor((1-alpha/2)*B)), beta - beta_diff(ceil(alpha/2*B))]
+
+F_inv_T_conf = F_inv_T - F_inv_T_diff(floor((1-alpha)*B))
+
+
+
 
